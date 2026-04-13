42004d2477566ea9cc015d2221d3572ee6bb831a
Thiago M. Dolabella <thiago.dmont@gmail.com>
2026-04-12 22:53:00 -0400

    feat(ui): migrate VirtualList to src/components/ui/

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 // Design system barrel
 export { Icon, type IconProps } from "./Icon";
 export { Resizable, type ResizableProps } from "./Resizable";
+export { VirtualList, type VirtualListProps, type VirtualListHandle } from "./VirtualList";
